f050889327354bf1c72146bd8b771f42247d2fb1
sandeep chugh <sandeep.chugh@live.com>
2020-08-13 19:50:02 -0500

    added swagger support

--- web/src/app.py
+++ web/src/app.py
@@ -1,13 +1,24 @@
-import json
-
 from flask import Flask, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
 
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.yaml'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "shop-catalog-api"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+
 @app.route("/", methods=['GET'])
 def get_available_apis():
-    return "<a href='/api/v1/catalog/products'>Products</a>"
+    return "<a href='/swagger'>Swagger</a>"
 
 
 @app.route("/api/v1/catalog/products", methods=['GET'])
@@ -25,9 +36,9 @@ def get_product_by_id(product_id):
     Gets all products
     :return:
     """
-    product = {"name":"test_product"}
+    product = {"name": "test_product"}
     return product
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
